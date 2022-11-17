9fef096300e1c909747ea01cf1fa1a7d98f25a81
Dylan LeBouef <dylanlebouef21@icloud.com>
2022-11-16 18:18:31 -0600

    add appending numbers instead of replacing

--- script.js
+++ script.js
@@ -18,13 +18,10 @@ const operate = function(a,b, operator) {
     return operator(a,b);
 };
 
-const appendNumber = function() {
-    const currentNumber = document.querySelector('.current-number');
-    const buttons = document.querySelectorAll('#btn');
+const currentNumber = document.querySelector('.current-number');
+const buttons = document.querySelectorAll('#btn');
+const displayValue = buttons.forEach(btn => btn.addEventListener('click', () => currentNumber.innerHTML += btn.value));
 
-    };
-
-
-    const currentNumber = document.querySelector('.current-number');
-    const buttons = document.querySelectorAll('#btn');
-    buttons.forEach(btn => btn.addEventListener('click', () => currentNumber.innerHTML = btn.value));
+const clear = function() {
+    currentNumber.innerHTML = '';
+};
