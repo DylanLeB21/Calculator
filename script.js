f1b50b31f022a5308af2e6331410312d8f04147a
Dylan LeBouef <dylanlebouef21@icloud.com>
2022-11-17 17:50:40 -0600

    add operator selectors, store value of first number upon click of an operator, add relevant functions to each operand via event listeners. probably wrong

--- script.js
+++ script.js
@@ -18,10 +18,29 @@ const operate = function(a,b, operator) {
     return operator(a,b);
 };
 
-const currentNumber = document.querySelector('.current-number');
+// add numbers to display, clear button
+let displayValue;
+let currentNumber = document.querySelector('.current-number');
 const buttons = document.querySelectorAll('#btn');
-const displayValue = buttons.forEach(btn => btn.addEventListener('click', () => currentNumber.innerHTML += btn.value));
+const clearBtn = document.getElementById('clear');
+buttons.forEach(btn => btn.addEventListener('click', () => displayValue = currentNumber.innerHTML += btn.value));
+clearBtn.addEventListener('click', () => displayValue = currentNumber.innerHTML = '');
 
-const clear = function() {
-    currentNumber.innerHTML = '';
-};
+// store value of display when an operator is chosen 
+let firstValue;
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => op.addEventListener('click', () => firstValue = displayValue));
+
+// selecting operators 
+const division = document.querySelector('.divide');
+const multiplication = document.querySelector('.multiply');
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.subtract');
+
+division.addEventListener('click', divide());
+multiplication.addEventListener('click', multiply());
+addition.addEventListener('click', add());
+subtraction.addEventListener('click', subtract());
+
+
+
