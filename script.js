e985bc5e663c0e62c51d6f32183d8e4b896d2a87
Dylan LeBouef <dylanlebouef21@icloud.com>
2022-11-18 19:02:30 -0600

    fix clear function and possiblly others not working because of function expression. changed to declaration

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-const add = function(a,b) {
-    return a + b;
-};
-
-const subtract = function(a,b) {
-    return a - b;
-};
-
-const multiply = function(a,b) {
-    return a * b;
-};
-
-const divide = function(a,b) {
-    return a / b;
-};
-
-const operate = function(a,b, operator) {
-    return operator(a,b);
-};
 
 // add numbers to display, clear button
 let displayValue;
 let currentNumber = document.querySelector('.current-number');
+let resultingNumber = document.querySelector('.resulting-number');
+
+
 const buttons = document.querySelectorAll('#btn');
-const clearBtn = document.getElementById('clear');
-buttons.forEach(btn => btn.addEventListener('click', () => displayValue = currentNumber.innerHTML += btn.value));
-clearBtn.addEventListener('click', () => displayValue = currentNumber.innerHTML = '');
+const clearBtn = document.getElementById('clear'); // might need to do array.from on these
+const deleteBtn = document.getElementById('backspace');
+const equalsBtn = document.getElementById('equals');
+
+
+buttons.forEach(btn => btn.addEventListener('click', () => currentNumber.textContent += btn.value));
+
+clearBtn.addEventListener('click', clear);
+
 
 // store value of display when an operator is chosen 
 let firstValue;
@@ -37,10 +27,33 @@ const multiplication = document.querySelector('.multiply');
 const addition = document.querySelector('.add');
 const subtraction = document.querySelector('.subtract');
 
-division.addEventListener('click', divide());
-multiplication.addEventListener('click', multiply());
-addition.addEventListener('click', add());
-subtraction.addEventListener('click', subtract());
+division.addEventListener('click', divide);
+multiplication.addEventListener('click', multiply);
+addition.addEventListener('click', add);
+subtraction.addEventListener('click', subtract)
 
 
+function clear() {
+    currentNumber.textContent = '';
+    resultingNumber.textContent = '';
+};
 
+function add(a,b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a - b;
+};
+
+function multiply(a,b) {
+    return a * b;
+};
+
+function divide(a,b) {
+    return a / b;
+};
+
+function operate(a,b, operator) {
+    return operator(a,b);
+};
